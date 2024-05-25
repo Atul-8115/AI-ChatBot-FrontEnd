0b22cb610f524376dff2abf88bcea2c529ad483c
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-25 16:35:11 +0530

    Pushing fronend code of my ChatBot app

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div
+              style={{
+                width: "100%",
+                // padding: 20,
+                minHeight: "20vh",
+                maxHeight: "30vh",
+                marginTop: 60,
+              }}
+            >
+                <p
+                  style={{fontSize: "30px", textAlign: "center"}}
+                >
+                    Built with love by Atul Pandey ❤️
+                </p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
